5ac84f969b34984fed368b84647482626d71c963
Kasheftin <Kasheftin@gmail.com>
2017-09-12 00:38:58 +0300

    Initial commit

---
+++ pages/search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" v-model="srch">
+    <div>The current query is: {{srch}}</div>
+    <div>The srch param in the url query is: {{querySrch}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    console.log('search rebuild?')
+    return {
+      srch: this.$router.currentRoute.query.srch,
+      querySrch: this.$router.currentRoute.query.srch
+    }
+  },
+  computed: {
+    routeQuery () {
+      return this.$router.currentRoute.query.srch
+    }
+  },
+  watch: {
+    routeQuery (querySrch) {
+      console.log('watch querySrch', querySrch)
+    },
+    srch (srch) {
+      console.log('watch srch', srch)
+      this.$router.replace({name: 'search', query: {srch: srch}})
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    console.log(to, from, next)
+    next()
+  }
+}
+</script>
